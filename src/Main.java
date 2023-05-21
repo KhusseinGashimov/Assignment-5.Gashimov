0fecaa50c0dec4a849e900c8d4e8b75c4f747ff0
KhusseinGashimov <rmit25@mail.ru>
2023-05-21 23:54:47 +0600

    commit7

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
+
+        bst.put(13, "Value 13");
+        bst.put(11, "Value 11");
+        bst.put(24, "Value 24");
+        bst.put(111, "Value 111");
+        bst.put(45, "Value 45");
+        bst.put(789, "Value 789");
+        bst.put(43, "Value 43");
+
+    }
+}
